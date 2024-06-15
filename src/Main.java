57ef502f33198f70fdf61e8c437cbafe58baea25
cyan1125 <79573210+cyan1125@users.noreply.github.com>
2024-06-15 02:34:39 +0000

    DB取得処理を監視クラスに移動

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,12 @@
-import model.Equipment;
-import model.Home;
 import model.Observation;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        var test = getHomes();
         while (true) {
             // 佐藤さん、鈴木さん
-            test.forEach(home -> {
-                var observation = new Observation(home);
-                if (observation.watch()) {
-                    // 滞在規定時間超過
-                    // 利用間隔規定時間超過
-                    System.out.println("異常です" + home.getId());
-                }
-            });
-
+            var observation = new Observation();
+            observation.watch();
             Thread.sleep(1000L); // TODO 実際には1分〜10分くらいでも良いのでは？
         }
 
@@ -29,27 +15,4 @@ public class Main {
         // 委譲検知の場合のみ通知
     }
 
-    public static List<Home> getHomes() {
-        List<Home> homes = new ArrayList<>();
-        homes.add(new Home("1",
-                List.of(new Equipment("1")),
-                60L,
-                720L,
-                        1,
-                LocalDateTime.of(1970, 1, 1, 0, 0)
-                )
-        );
-
-        homes.add(new Home("2",
-                List.of(new Equipment("2")),
-                90L,
-                1440L,
-                2,
-                LocalDateTime.of(1970, 1, 1, 0, 0)
-                )
-        );
-
-
-        return homes;
-    }
 }
--- src/model/Observation.java
+++ src/model/Observation.java
@@ -2,41 +2,39 @@ package model;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Observation {
-    /** 家庭 */
-    private Home home;
 
-    public Observation(Home home) {
-        this.home = home;
-    }
+    public void watch() {
+        List<Home> homes = getHomes();
 
-    public boolean watch() {
+        for (Home home : homes) {
 
-        if (home.isMonitoredTarget()) {
-            home.modifiedLastMonitoringTime();
-
-            return exceedingTheStipulatedStayTime();
+            if (home.isMonitoredTarget()) {
+                home.modifiedLastMonitoringTime();
+                if (exceedingTheStipulatedStayTime(home)) {
+                    System.out.println("異常です" + home.getId());
+                }
+            }
         }
 
-        return false;
-
-//        usageIntervalRegulationTime();
+        // usageIntervalRegulationTime();
     }
 
     /**
      * 滞在規定時間超過
+     * 
      * @return
      */
-    private boolean exceedingTheStipulatedStayTime(){
+    private boolean exceedingTheStipulatedStayTime(Home home) {
         List<EntryExitRecord> latestEntryExitRecords = home.getEntryTimeForThoseWhoHaveNotLeftTheRoom();
 
         for (EntryExitRecord latestEntryExitRecord : latestEntryExitRecords) {
             long elapsedTime = ChronoUnit.MINUTES.between(
                     latestEntryExitRecord.getEntryDateTime(),
-                    LocalDateTime.now()
-            );
+                    LocalDateTime.now());
 
             if (home.getRegulationOfStayMinute() <= elapsedTime) {
                 return true;
@@ -48,9 +46,29 @@ public class Observation {
 
     /**
      * 利用間隔規定時間超過
+     * 
      * @return
      */
     private boolean usageIntervalRegulationTime() {
         return true;
+    }
+
+    public static List<Home> getHomes() {
+        List<Home> homes = new ArrayList<>();
+        homes.add(new Home("1",
+                List.of(new Equipment("1")),
+                60L,
+                720L,
+                1,
+                LocalDateTime.of(1970, 1, 1, 0, 0)));
+
+        homes.add(new Home("2",
+                List.of(new Equipment("2")),
+                90L,
+                1440L,
+                2,
+                LocalDateTime.of(1970, 1, 1, 0, 0)));
+
+        return homes;
     }
 }
